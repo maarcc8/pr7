520317ac483487da68761e3d235ecbdd4d49a372
maarcc <maarcc@maarcc-X555LAB>
2015-11-04 20:36:02 +0100

    Cambio final

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -1,25 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pr7app;
 
-/**
- *
- * @author ivan
- */
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Pr7app {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException, IllegalStateException, FTPIllegalReplyException, FTPException, FileNotFoundException, FTPDataTransferException, FTPAbortedException {
 
         URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
         url.openConnection();
         InputStream reader = url.openStream();
-        FileOutputStream writer = new FileOutputStream("maarcc");
+        FileOutputStream writer = new FileOutputStream("/home/maarcc/Escritorio/marc.cuadrado.pdf");
         byte[] buffer = new byte[153600];
         int bytesRead = 0;
         while ((bytesRead = reader.read(buffer)) > 0) {
@@ -30,9 +30,9 @@ public class Pr7app {
         reader.close();
 
         FTPClient client = new FTPClient();
-        client.connect("ftp.host.com");
-        client.login("carlo", "mypassword");
-        client.upload(new java.io.File("localFile.ext"));
+        client.connect("srv.toca.cat");
+        client.login("fulano", "Platano123");
+        client.upload(new java.io.File("/home/maarcc/Escritorio/marc.cuadrado.pdf"));
         client.disconnect(true);
 
     }
