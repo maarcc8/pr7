401fdbcaa917b871c5148f3be7311b9ad9fc2c6b
maarcc8 <>
2015-11-04 20:06:16 +0100

    Primer cambio

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -15,9 +15,26 @@ public class Pr7app {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Hi world!");
+
+        URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
+        url.openConnection();
+        InputStream reader = url.openStream();
+        FileOutputStream writer = new FileOutputStream("maarcc");
+        byte[] buffer = new byte[153600];
+        int bytesRead = 0;
+        while ((bytesRead = reader.read(buffer)) > 0) {
+            writer.write(buffer, 0, bytesRead);
+            buffer = new byte[153600];
+        }
+        writer.close();
+        reader.close();
+
+        FTPClient client = new FTPClient();
+        client.connect("ftp.host.com");
+        client.login("carlo", "mypassword");
+        client.upload(new java.io.File("localFile.ext"));
+        client.disconnect(true);
+
     }
-    
+
 }
